40d7586a91a0c237498f3e2b9ed71fe00b718453
Deniz Saganak <saganak.deniz@gmail.com>
2024-05-20 14:38:26 +0200

    feat/create ProjectList component and show data

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,9 +1,13 @@
+import ProjectList from "../components/ProjectList";
 import { MOCK_PROJECTS } from "../data/MockProjects";
 
 
 export default function ProjectsPage() {
 
   return (
-    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+    <>
+      <h1>PROJECTS</h1>
+      <ProjectList projects={MOCK_PROJECTS} />
+    </>
   )
 }
