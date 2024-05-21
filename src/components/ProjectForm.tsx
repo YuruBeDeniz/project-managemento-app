d164337674aa72463929fbd02f9645de1f39d5c5
Deniz Saganak <saganak.deniz@gmail.com>
2024-05-21 15:50:18 +0200

    feat/simplify handleChange function

--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -2,68 +2,54 @@ import { SyntheticEvent, useState } from "react";
 import { Project } from "../models/Project";
 
 type ProjectFormProps = {
-  project: Project;
-  onCancel: () => void;
-  onSave: (project: Project) => void;
+    project: Project;
+    onCancel: () => void;
+    onSave: (project: Project) => void;
 }
 
 export default function ProjectForm({ onCancel, onSave, project: initialProject }: ProjectFormProps) {
-  const [project, setProject] = useState<Project>(initialProject);
- 
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault();
-    onSave(project)
+    const [name, setName] = useState<string>(initialProject.name);
+    const [description, setDescription] = useState<string>(initialProject.description);
+    const [budget, setBudget] = useState<number>(initialProject.budget);
+    const [isActive, setIsActive] = useState<boolean>(initialProject.isActive);
+
+    const handleSubmit = (event: SyntheticEvent) => {
+      event.preventDefault();
+      const updatedProject = new Project ({
+          ...initialProject, 
+          name,             
+          description,
+          budget,
+          isActive
+      });
+      onSave(updatedProject);
   };
 
-  const handleChange = (event: any) => {
-    const { type, name, value, checked } = event.target;
+    return (
+        <form className="input-group vertical" onSubmit={handleSubmit}>
+            <label htmlFor="name">Project Name</label>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="enter name" />
 
-    let updatedValue = type === 'checkbox' ? checked : value;
+            <label htmlFor="description">Project Description</label>
+            <textarea value={description} onChange={(e) => setDescription(e.target.value)} name="description" placeholder="enter description" />
 
-    if (type === 'number') {
-      updatedValue = Number(updatedValue);
-    };
+            <label htmlFor="budget">Project Budget</label>
+            <input value={budget} onChange={(e) => setBudget(parseFloat(e.target.value))} type="number" name="budget" placeholder="enter budget" />
+            
+            <label htmlFor="isActive">Active?</label>
+            <input checked={isActive} onChange={(e) => setIsActive(e.target.checked)} type="checkbox" name="isActive" />
 
-    const change = {
-      [name]: updatedValue,
-    };
-
-    let updatedProject: Project;
-
-    setProject((p) => {
-      updatedProject = new Project({ ...p, ...change });
-      return updatedProject;
-    })
-  }
-
-  return (
-  <form 
-    className="input-group vertical"
-    onSubmit={handleSubmit}
-  >
-    <label htmlFor="name">Project Name</label>
-    <input value={project.name} onChange={handleChange} type="text" name="name" placeholder="enter name" />
-
-    <label htmlFor="description">Project Description</label>
-    <textarea value={project.description} onChange={handleChange} name="description" placeholder="enter description" />
-
-    <label htmlFor="budget">Project Budget</label>
-    <input value={project.budget} onChange={handleChange} type="number" name="budget" placeholder="enter budget" />
-
-    <label htmlFor="isActive">Active?</label>
-    <input checked={project.isActive} onChange={handleChange} type="checkbox" name="isActive" />
-
-    <div className="input-group">
-      <button className="primary bordered medium">Save</button>
-      <span />
-      <button 
-        onClick={onCancel}
-        type="button" 
-        className="secondary bordered medium"
-      >
-        Cancel
-      </button>
-    </div>
-  </form> 
-  )
+            <div className="input-group">
+                <button className="primary bordered medium">Save</button>
+                <span />
+                <button 
+                    onClick={onCancel}
+                    type="button" 
+                    className="secondary bordered medium"
+                >
+                    Cancel
+                </button>
+            </div>
+        </form>
+    );
 }
