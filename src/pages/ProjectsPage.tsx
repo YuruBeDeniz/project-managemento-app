8c6e71e733c3ba7acc2127e51d8525fef3777d76
Deniz Saganak <saganak.deniz@gmail.com>
2024-05-22 14:27:39 +0200

    feat/add API

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProjectList from "../components/ProjectList";
-import { MOCK_PROJECTS } from "../data/MockProjects";
 import { Project } from "../models/Project";
+import { projectAPI } from "../data/projectAPI";
 
 
 export default function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(1);
+        setError("");
+        setProjects(data);
+      }
+      catch (e) {
+        if(e instanceof Error) {
+          setError(e.message);
+        }
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+    loadProjects();
+  }, [])
 
   const saveProject = (project: Project) => {
     let updatedProjects = projects.map((p: Project) => {
@@ -17,7 +39,28 @@ export default function ProjectsPage() {
   return (
     <>
       <h1>PROJECTS</h1>
+
+      {error && (
+        <div className="row">
+        <div className="card large error">
+          <section>
+            <p>
+              <span className="icon-alert inverse "></span>
+              {error}
+            </p>
+          </section>
+        </div>
+      </div>
+      )}
+
       <ProjectList projects={projects} onSave={saveProject} />
+
+      {loading && (
+        <div className="center-page">
+        <span className="spinner primary"></span>
+        <p>Loading...</p>
+      </div>
+      )}
     </>
   )
 }
